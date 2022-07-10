b079e8d6aa7b22f3ef3b96219f09add9f210b500
padmaja095 <dpadmaja.95@gmail.com>
2022-07-11 02:51:31 +0530

    array solved problem

--- src/ds/array/PartitionEqualSubsetSum.java
+++ src/ds/array/PartitionEqualSubsetSum.java
@@ -1,19 +1,22 @@
 package ds.array;
 
-
 public class PartitionEqualSubsetSum {
     public static void main(String[] args) {
-        int arr[] = { 2,3,4,1, 5, 5 };
+        int arr[] = { 2, 3, 4, 1, 5, 5 };
         PartitionEqualSubsetSum ms = new PartitionEqualSubsetSum();
-        System.out.println(ms.partitionEqualSubsetSum(arr)+"is the starting index where the subset sum is partitioned.");
+      //  System.out.println(ms.partitionEqualSubsetSumFirstMethod(arr)
+        //        + "is the starting index where the subset sum is partitioned.");
+                System.out.println(ms.partitionEqualSubsetSumSecondMethod(arr)
+                + "is the starting index where the subset sum is partitioned.");
+                
     }
 
-    private int partitionEqualSubsetSum(int[] arr) {
+    private int partitionEqualSubsetSumFirstMethod(int[] arr) {
         int i = 0;
-        int j = arr.length-1;
-        int leftSum =arr[0] ;
+        int j = arr.length - 1;
+        int leftSum = arr[0];
         int rightSum = arr[arr.length - 1];
-        while (i < j-1) {
+        while (i < j - 1) {
             if (leftSum > rightSum) {
                 j--;
                 rightSum += arr[j];
@@ -21,12 +24,34 @@ public class PartitionEqualSubsetSum {
             } else if (leftSum < rightSum) {
                 i++;
                 leftSum += arr[i];
-            } else{
+            } else {
                 System.out.println("full array is not processed.");
-                i++;j--;
+                i++;
+                j--;
             }
 
         }
         return i;
+    }
+
+    private int partitionEqualSubsetSumSecondMethod(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        int leftSum=0;int rightSum=sum;
+        for(int i=0;i<arr.length;i++)
+        { 
+            leftSum+=arr[i];
+            rightSum-=arr[i];
+            
+           if(leftSum==rightSum)
+           {
+            return i;
+           }
+         
+
+        }
+        return -1;
     }
 }
